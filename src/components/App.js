be007badc270a03498e3c07f71d723e465a533ab
joycewangui <joycewangui590@gmail.com>
2024-08-25 21:01:37 +0300

    Information flow lab

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
+
+
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
